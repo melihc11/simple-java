d24d6e5a61d91c34784dc23459a92c64b6855fd0
Melih Cifci <59452321+melihc11@users.noreply.github.com>
2023-09-29 15:12:20 -0400

    Add files via upload

---
+++ input.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    /* 
+    
+    Basic Usage of Loops
+    ---------------------
+    printf("How many times should I repeat this sentence? ");
+    int n;
+    scanf("%d", &n);
+    for (int i=0; i<n; ++i){
+        printf("I am repeating it %d times\n", n)
+
+
+    Basic Conditional Input Question 
+    ---------------------------------
+    printf("How much cars does Cafar have? ");
+    float n;
+    scanf("%f", &n);
+    if (n==2) {
+        printf("Correct, Cafer has %f cars\n", n); 
+    }
+    if (n>2){
+        printf ("Incorrect, He's not that rich yet");
+    }
+    if (n<2){
+        printf ("Try again, he's wealthier than that ");
+    }
+
+    */
+
+    return 0; 
+}
+
